7b1fd9a5ec49b6d2e627e9f42e66b878400b54a6
Glendon <glendon.small@chestertons.com>
2023-10-26 12:09:46 +0100

    Testing CircleCi Heroku deployment

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 class AuthorController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Display a listing of the resource
      */
     public function index()
     {
