7fb5ad700310843c6aa4dfb3d379506975b0a85e
Glendon <glendon.small@chestertons.com>
2023-10-28 14:47:10 +0100

    switch to filaments test

--- app/Filament/Widgets/StatsAdminOverview.php
+++ app/Filament/Widgets/StatsAdminOverview.php
@@ -19,12 +19,12 @@ class StatsAdminOverview extends BaseWidget
                 ->chart([7, 2, 10, 3, 15, 4, 17])
                 ->color('success'),
             Stat::make('Teams', Team::query()->count())
-                ->description('All teams from the database')
+                ->description('All teams from the database.')
                 ->descriptionIcon('heroicon-m-arrow-trending-down')
                 ->color('danger'),
             Stat::make('Employees', Employee::query()->count())
                 ->description('All employees from the database')
-                ->descriptionIcon('heroicon-m-arrow-trending-up')
+                ->descriptionIcon('heroicon-m-arrow-trending-up.')
                 ->color('success'),
         ];
     }
